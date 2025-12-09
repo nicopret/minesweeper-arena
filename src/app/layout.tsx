309ec31456d1e699d51b8fbac9c4579173b7a54a
Nico Pretorius <npretorius@scottlogic.com>
2025-12-09 15:14:00 +0000

    update code with prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const metadata = {
-  title: 'Minesweeper Game - Classic Puzzle Game',
-  description: 'Play the classic Minesweeper game online. Test your logic and strategy skills with three difficulty levels.',
-  keywords: 'minesweeper, puzzle game, logic game, strategy game, online game',
+  title: "Minesweeper Game - Classic Puzzle Game",
+  description:
+    "Play the classic Minesweeper game online. Test your logic and strategy skills with three difficulty levels.",
+  keywords: "minesweeper, puzzle game, logic game, strategy game, online game",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
